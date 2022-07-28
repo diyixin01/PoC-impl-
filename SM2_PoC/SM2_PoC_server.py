4e626879945595c9fac1b44969b1a3ff6717660f
liu <75195549+diyixin01@users.noreply.github.com>
2022-07-28 21:07:49 +0800

    Update SM2_PoC_server.py

--- SM2_PoC/SM2_PoC_server.py
+++ SM2_PoC/SM2_PoC_server.py
@@ -1,44 +1,44 @@
 import sm3
-import sys
 import json
-import random
 import socket
-from os.path import commonprefix
 
-d = 7
-n = 23
+
 HOST = ''
-PORT = 1234
+PORT = 8050
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.bind((HOST, PORT))
 
-print("Server connected!")
+print("客户端链接成功")
 
-# 从客户端接收数据
+
+
+
+
+d = 7
+n = 23
 k,addr = client.recvfrom(1024)
 k = int(k.decode(),16)
 v,addr = client.recvfrom(1024)
 v = int(v.decode(),16)
 
-# create key-value table
 table = []
 for i in range(20000 ,20000 + pow(2,16)):
     h = sm3._hash(str(i) + str(i))
     table.append(h)   
-V = []
+Q = []
 for m in table:
     if m[:2] == k:
-        V.append((pow(int(m,16),d))%n)
+        Q.append((pow(int(m,16),d))%n)
 
-print("V:",V)
+print("Q:",Q)
 
 # 计算H_ab
 H_ab=(pow(v,d))%n
 
-# 向客户端发送H_ab与data set
+
 client.sendto(hex(H_ab).encode(),addr)
 json_v = json.dumps(V)
 client.sendto(json_v.encode('utf-8'),addr)
 client.close()
 
-print("Server closed!")
+print("服务器端关闭")
